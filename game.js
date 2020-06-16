af9502580a7cc90ebed4e625545e76a46696f4b0
joseph-jennings1 <40963397+joseph-jennings1@users.noreply.github.com>
2020-06-15 17:14:54 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,185 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 627
+canvas.height = 627;
+document.body.appendChild(canvas);
+
+// Background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "images/maze2.gif";
+
+// Hero image
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "images/hero.png";
+
+// Monster image
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function () {
+	monsterReady = true;
+};
+monsterImage.src = "images/monster.png";
+
+// Game objects
+var hero = {
+	speed: 256 // movement in pixels per second
+};
+var monster = {};
+var monstersCaught = 0;
+var monstersLeft = 10;
+var currentScore = 0;
+var highScore = 0;
+var collision = 0;
+
+// Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Reset the game when the player catches a monster
+var reset = function () {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+//change this to 'clearMonster' after adding more monster sprites for gameplay
+var resetMonster = function() {
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));	
+}
+
+// Update game objects
+var update = function (modifier) {
+	if (38 in keysDown) { // Player holding up
+		if (hero.y - (hero.speed * modifier) > 0) {
+			hero.y -= hero.speed * modifier;
+			edgeDetection();
+			if (collision == 1) {
+				hero.y += hero.speed * modifier;
+				collision = 0;
+			}
+		}
+		
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
+	}
+
+	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		currentScore = monstersCaught*10;
+		if (currentScore > highScore) {
+			highScore = currentScore;
+		}
+		--monstersLeft;
+		if (monstersLeft == 0) {
+			//resetGame
+			//monstersLeft = 10;
+			//currentScore = 0;
+		}
+		//change to a 'clearMonster' function after adding more monsters
+		resetMonster();
+	}
+};
+
+//Edge detection
+var edgeDetection = function () {
+	var checkedImage = ctx.getImageData(x, y, 15, 15);
+	var checkedPixel = checkedImage.data;
+	for (var i = 0; j = checkedPixel.length, i < j; i += 4) {
+		if (checkedPixel[i] == 51) {
+			collision = 1;
+		}
+	}
+};
+
+// Draw everything
+var render = function () {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+	
+	// Current Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Current score: " + currentScore, 16, 600);
+	
+	// Monsters left
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Monsters left: " + monstersLeft, 230, 600);
+	
+	// High Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("High score: " + highScore, 435, 600);
+};
+
+// The main game loop
+var main = function () {
+	var now = Date.now();
+	var delta = now - then;
+
+	update(delta / 1000);
+	render();
+
+	then = now;
+
+	// Request to do this again ASAP
+	requestAnimationFrame(main);
+};
+
+// Cross-browser support for requestAnimationFrame
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+// Let's play this game!
+var then = Date.now();
+reset();
+main();
